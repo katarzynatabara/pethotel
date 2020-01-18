fbeae628450be84b36dd79c4029bee8dbd3d6d7c
Kasia <katarzynatabara35@gmail.com>
2020-01-18 14:50:13 +0100

    konfiguracja firebase
    dodano skrypty

--- frontend/src/config/localhost.js
+++ frontend/src/config/localhost.js
@@ -1,4 +1,13 @@
-const firebaseConfig = <insert firebase config here>;
+const firebaseConfig ={
+  apiKey: "AIzaSyCrkGXvcYVanX7pKgZROwjYg1cwXTdEaLs",
+  authDomain: "pet-hotel-prod-bf930.firebaseapp.com",
+  databaseURL: "https://pet-hotel-prod-bf930.firebaseio.com",
+  projectId: "pet-hotel-prod-bf930",
+  storageBucket: "pet-hotel-prod-bf930.appspot.com",
+  messagingSenderId: "887929759226",
+  appId: "1:887929759226:web:e93356642b7f37cbedb043",
+  measurementId: "G-RMQGSCTJ70"
+};
 
 // Cloud Functions
 // const backendUrl = `http://localhost:5000/${
